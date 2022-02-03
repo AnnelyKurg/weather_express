f1ec9979a2db40fbf17ec21555311115338420e1
Annely Kurg <annely.kurg@khk.ee>
2022-02-03 11:30:50 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const key = 'feabf737c0396f2a57dddb353e18684c'
 let city = 'Tartu'
+
 
 app.get('/', function (req, res){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
@@ -25,8 +29,29 @@ app.get('/', function (req, res){
     res.render('index', {
         description: description,
         city: city,
-        temp: temp,
+        temp: temp
         })
    })
 })
+
+app.post('/', function (req, res){
+    /*console.log(req.body)
+    res.redirect('/')*/
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+    })
+
 app.listen(3000)
